65eff3351d3adeb8c4c645a020e736c8e71aa194
Omkar Dhomane <47618402+dhomane@users.noreply.github.com>
2023-04-13 20:21:14 +0530

    Update and rename iserver-stop.sh to tomcat-iserver-stop.sh

--- tomcat-iserver-stop.sh
+++ tomcat-iserver-stop.sh
@@ -1,13 +1,19 @@
 #/bin/bash
 
 
+# Stop Tomcat
+sudo service mstr tomcatstop
+
+sleep 5
+
+# Stop I-Server
 
 sudo service mstr iserverstop
+
 
 sleep 5
 
 sudo kill -9 `ps aux|grep MSTRSvr|grep -v 'grep'|awk '{print $2}'`
-
 
 
 sleep 5
