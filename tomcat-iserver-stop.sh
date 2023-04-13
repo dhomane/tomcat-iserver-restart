cce37a8e629bd6bb82c675f4951478200344f52f
Omkar Dhomane <47618402+dhomane@users.noreply.github.com>
2023-04-13 21:26:16 +0530

    Update tomcat-iserver-stop.sh

--- tomcat-iserver-stop.sh
+++ tomcat-iserver-stop.sh
@@ -4,16 +4,16 @@
 # Stop Tomcat
 sudo service mstr tomcatstop
 
-sleep 5
+sleep 10
 
 # Stop I-Server
 
 sudo service mstr iserverstop
 
 
-sleep 5
+sleep 10
 
 sudo kill -9 `ps aux|grep MSTRSvr|grep -v 'grep'|awk '{print $2}'`
 
 
-sleep 5
+sleep 10
