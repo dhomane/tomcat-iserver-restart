bd2552713a9cf678c755b727d173c4396803a692
Omkar Dhomane <47618402+dhomane@users.noreply.github.com>
2023-04-20 15:56:36 +0530

    Update iserver-start.sh

--- iserver-start.sh
+++ iserver-start.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 
-# Start I-Server
 
-sudo service mstr iserverstart
-
-
-sleep 10
-
-while true;
-do
-
-if sudo service mstr status | grep "Intelligence Server is in running status";
-  then iserver_status=1
-else iserver_status=0
-fi
-
-if [[ $iserver_status -eq 1 ]];
-   then break
-fi
-done
-
+echo " `date` Test "
 
 
 
