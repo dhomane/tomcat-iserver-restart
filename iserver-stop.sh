0aa1d8f3a73e8452d540f944eaa62f26e5dc5b95
Omkar Dhomane <47618402+dhomane@users.noreply.github.com>
2023-04-20 15:56:49 +0530

    Update iserver-stop.sh

--- iserver-stop.sh
+++ iserver-stop.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-# Stop I-Server
 
-sleep 120
-
-sudo service mstr iserverstop
-
-
-sleep 10
-
-sudo kill -9 `ps aux|grep MSTRSvr|grep -v 'grep'|awk '{print $2}'`
+echo " `date` Test "
 
 
 sleep 10
